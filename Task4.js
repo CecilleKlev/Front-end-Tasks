cae4b8a49627f66ddcaf75bad823ac0a5ec800fd
CecilleKlev <cecilleklev@gmail.com>
2023-09-18 22:15:10 +0200

    Added Task4.js file and some function code

---
+++ Task4.js
@@ -0,0 +1,8 @@
+//create button element
+var button = document.createElement('button');
+button.textContent ='Read Article';
+
+//Add the button click event listener
+button.addEventListener('click', function(){
+    console.log('README');
+});
