c9fcada6a2496048807382979f0c0a1cf771ca6b
CecilleKlev <cecilleklev@gmail.com>
2023-09-24 14:08:57 +0200

    comments

--- Task1.js
+++ Task1.js
@@ -1,14 +1,17 @@
+//Variable to store ID of the interval timer creat with setInterval
 var timerId;
-
+//Select HTML element with the id "content" and store it on variable "c", to manipulate the style of the element
 var c = document.getElementById("content");
 
-function startCycle(){
-    var randomInterval = Math.floor(Math.random()* (1800 -1+1))+1;
+function startCycle(){ //function that initiates the color-changing cycle 
+    var randomInterval = Math.floor(Math.random()* (1800 -1+1))+1; 
     timerId = setInterval(changeColor, randomInterval * 1000); //convert seconds to milliseconds
 }
 
+//function that changes the text color stored in "c"
 function changeColor(){
     c.style.color= "red";
 }
 
+//code starts by starting timer and then change color
 startCycle();
